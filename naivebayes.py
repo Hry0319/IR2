1eec11904097f0a577e4c418f14ca9c89896f7ae
JayHsieh <k3331863@gmail.com>
2016-05-29 22:47:59 +0800

    + evaluation function and py script to scoring
    X ~5% score very bad

--- naivebayes.py
+++ naivebayes.py
@@ -64,7 +64,7 @@ def main():
             TopicList.append(topic)
             # topic.SQL_SUM()
             TotalWords += topic.TopicWordsCount
-    print TotalWords
+    # print TotalWords
 
     #
     #  Cal each Topics' Probability     ( sum of these Probability is 1.0 )
@@ -83,10 +83,17 @@ def main():
     AnswerList       = []
     getFileList(TestDataPath, TestDataFileList)
 
-    for path in TestDataFileList[0:5]:    #test debug
+    for path in TestDataFileList[0:]:    #test debug
         AnswerList.append( Classifier(path, TopicList) )
 
-    print AnswerList
+    # print AnswerList
+
+    #
+    # write answer list to the output.txt
+    #
+    # WriteOutput(OutPutFile, AnswerList)
+
+    evaluation(AnswerList)
 
 
 def Classifier(path, TopicList):
@@ -111,7 +118,7 @@ def Classifier(path, TopicList):
 
     SimilarClass = ""
     tmpL         = 0.0
-    for topic in TopicList:#[:1]:    #test debug
+    for topic in TopicList[0:]:    #test debug
         Likelihood = 0.0
         Select = []
         for TestDataUni in TestDataUnigramList:  #對每個字去db找data
@@ -120,17 +127,18 @@ def Classifier(path, TopicList):
             #--------------------------------------------
             # 1 LIST
             #
+
             Count = topic.UnigramCount.get(TestDataUni)
 
-            if Count != None:
-                Likelihood += np.log(float(Count) / topic.VocabCount)  # ? 235867
+
+            if Count != None and Count > 2:
+                Likelihood += np.log(float(Count) / len(TestDataUnigramList))  # ? 235867)
 
         # print Likelihood
 
-        Likelihood = Likelihood - np.log(topic.TopicProbability)
+        Likelihood = 1*( Likelihood - np.log (topic.TopicProbability) )
 
-        print Likelihood
-
+        # print Likelihood
 
             #
             #--------------------------------------------
@@ -143,9 +151,7 @@ def Classifier(path, TopicList):
             #         Likelihood += np.log( sel )  # TopicProbability
             #     # NaiveBayes
             # Likelihood -= np.log(topic.TopicProbability)
-
-        
-
+ 
         if tmpL == 0:
             tmpL = Likelihood
             SimilarClass = topic.Label
@@ -153,20 +159,24 @@ def Classifier(path, TopicList):
             tmpL = Likelihood
             SimilarClass = topic.Label
 
-    return SimilarClass
+    return SimilarClass   # return the label name of the file from test
 
 
+# def NaiveBayesProbability(Class, Feature):
 
+#     return
 
-
-
-
-
-    return  # return the label name of the file from test
-
-
-
-def NaiveBayesProbability(Class, Feature):
+def WriteOutput(path, oList):
+    f = open(path, 'wb+')    
+    f.seek(0)
+    count = 0
+    for _class in oList:
+        f.write(str(count+1) + ' ' + str(oList[count]) + '\n')
+        count+=1
+        # if count > 50:
+            # break
+    f.flush()
+    f.close()
 
     return
 
@@ -197,6 +207,27 @@ def getFileList(path, FileList):
         if not item.startswith('.') and os.path.isfile(os.path.join(path, item)):
             FileList.append(path + item)
 
+
+
+def evaluation(output):
+    score = 0
+
+    f = open('ans.test.txt')
+    ans = f.readlines()
+    f.close()
+
+    # f = open('output.txt')
+    # output = f.readlines()
+    # f.close()
+
+
+    for i in xrange(0, len(output)):
+        if ans[i].strip('\n').split(' ')[1] == output[i]:
+            score+=1
+
+    print "\n!!   score : %d   !!\n" % score
+
+    return
 
 # main()
 if __name__ == '__main__':
